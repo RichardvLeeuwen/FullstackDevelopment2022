92454086eb504a8f5259ddf4494a183ae4881340
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2022-02-03 03:16:16 +0200

    Finished exercises 2.1-2.5

---
+++ Part2/courseinfo/src/components/Course.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const sumFunc = (previous, current) => previous + current.exercises
+  const totalExercises = parts.reduce(sumFunc, 0)
+  return (
+    <div>
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+
+export default Course
