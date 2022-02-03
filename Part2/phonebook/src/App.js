f05ed8197cbed0137d4114ceb1683de5ba38d19a
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2022-02-04 01:41:26 +0200

    finished 2.9

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,33 +1,61 @@
 import { useState } from 'react'
 
-const Person = ({ name }) => {
+const Person = ({ name, number }) => {
   return (
     <div>
       <p>
-        {name}
+        {name} {number}
       </p>
     </div>
   )
 }
 
-const Form = ({submitFunc, inputValue, inputChangeFunc, inputDesc, buttonDesc}) => {
+const Header = ({header}) => {
+  return (
+    <div>
+      <h2>{header}</h2>
+    </div>
+  )
+}
+
+const PhoneForm = ({submitFunc, inputNameValue, inputNameChangeFunc,inputPhoneValue, inputPhoneChangeFunc }) => {
+  const formTitle = "Add a new person"
+
   return (
     <form onSubmit={submitFunc}>
+      <Header header={formTitle}/>
       <div>
-        {inputDesc}: <input value={inputValue} onChange={inputChangeFunc}/>
+        name: <input value={inputNameValue} onChange={inputNameChangeFunc}/>
+        <br></br>
+        number: <input value={inputPhoneValue} onChange={inputPhoneChangeFunc}/>
       </div>
       <div>
-        <button type="submit">{buttonDesc}</button>
+        <button type="submit">add</button>
       </div>
     </form>
   )
 }
 
+const PhoneFilter = ({inputNameValue, inputNameChangeFunc }) => {
+  const formTitle = "filter shown with"
+
+  return (
+    <div>
+      {formTitle} <input value={inputNameValue} onChange={inputNameChangeFunc}/>        
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  const phonebookTitle = "Phonebook"
+  const numbersTitle = "Numbers"
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,22 +64,44 @@ const App = () => {
       return
     }
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const showPersons = () => {
+    if(newFilter==='') {
+      return persons
+    }
+    else {
+      return persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    }
+  } 
+
+  const filteredPeople = showPersons()
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Form submitFunc={addPerson} inputValue={newName} inputChangeFunc={handleNameChange} inputDesc='name' buttonDesc='add'/>
-      <h2>Numbers</h2>
-      {persons.map((person) => <Person key={person.name} name={person.name}/>)}
+      <Header header={phonebookTitle}/>
+      <PhoneFilter inputNameValue={newFilter} inputNameChangeFunc={handleFilterChange}/>
+      <PhoneForm submitFunc={addPerson} inputNameValue={newName} inputNameChangeFunc={handleNameChange} inputPhoneValue={newNumber} inputPhoneChangeFunc={handleNumberChange} />
+      <Header header={numbersTitle}/>
+      {filteredPeople.map((person) => <Person key={person.name} name={person.name} number={person.number}/>)}
     </div>
   )
 }
