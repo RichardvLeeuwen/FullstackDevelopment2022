c5bf66ce47149665c78c4dfca9e619ced7d9bdb8
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2022-02-04 00:32:44 +0200

    exercise 2.6 done

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,24 +1,53 @@
 import { useState } from 'react'
 
+const Person = ({ name }) => {
+  return (
+    <div>
+      <p>
+        {name}
+      </p>
+    </div>
+  )
+}
+
+const Form = ({submitFunc, inputValue, inputChangeFunc, inputDesc, buttonDesc}) => {
+  return (
+    <form onSubmit={submitFunc}>
+      <div>
+        {inputDesc}: <input value={inputValue} onChange={inputChangeFunc}/>
+      </div>
+      <div>
+        <button type="submit">{buttonDesc}</button>
+      </div>
+    </form>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Form submitFunc={addPerson} inputValue={newName} inputChangeFunc={handleNameChange} inputDesc='name' buttonDesc='add'/>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => <Person key={person.name} name={person.name}/>)}
     </div>
   )
 }
