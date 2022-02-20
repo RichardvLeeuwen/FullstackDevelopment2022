3c3865019012561f6ee89c52738020c9c7d3bdfc
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2022-02-21 01:55:17 +0200

    finished 4.7

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -29,9 +29,33 @@ const mostBlogs = (blogs) => {
   return {author, mostBlogs}
 }
 
+const mostLikes = (blogs) => {
+  for (let i = 0; i < blogs.length; i++) {
+    for (let j = 0; j < blogs.length; j++ ) {
+      if(blogs[i].author === blogs[j].author && i !== j && blogs[i].likes !== 0 && blogs[j].likes !==0) {
+        blogs[i].likes = blogs[i].likes + blogs[j].likes
+        blogs[j].likes = 0
+      }
+    }
+  }
+  let x = 0
+  let max = 0
+  for (let i = 0; i < blogs.length; i++) {
+    if(blogs[i].likes > max) {
+      max = blogs[i].likes
+      x = i
+    }
+  }
+  const mAuthor = blogs[x].author
+  const mLikes = blogs[x].likes
+  const ob = {author : mAuthor, likes: mLikes }
+  return ob
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
