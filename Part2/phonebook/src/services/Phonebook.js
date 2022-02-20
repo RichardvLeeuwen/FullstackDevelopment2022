4e27b21ac0693d578778e20fa0ad52656ecb5c59
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2022-02-20 18:24:03 +0200

    start 4.1

--- Part2/phonebook/src/services/Phonebook.js
+++ Part2/phonebook/src/services/Phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const url = 'http://localhost:3001/persons'
+const url = '/api/persons'
 
 //as instructed in 2.16, similar as what was shown in the example. Due to a react warning I assigned the default exports to a variable first.
 const getAllPersons = () => {
