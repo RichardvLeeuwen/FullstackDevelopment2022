4e9860de130aa415a326bc2d69b5298fafbb9409
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2022-02-21 16:15:10 +0200

    finished 4.18

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config= require('./utils/config')
 const logbook = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -19,6 +20,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHndler)
 
