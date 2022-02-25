3284b8195dc3395004b629aa7ff793e698fbc801
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2022-02-25 14:31:23 +0200

    Finished 5.2

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -16,6 +16,15 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => { //as given in the tutorial in part 5
+    const loggedUserJSON = window.localStorage.getItem('blogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)      
+      setUser(user)
+      //noteService.setToken(user.token)
+    }
+  }, [])
+
   const handleNameChange = (event) => {
     setNewUsername(event.target.value)
   }
@@ -27,11 +36,16 @@ const App = () => {
   const handleLogin = async (event) => {
     event.preventDefault()
     const user = await loginService.login({ username, password })
-
+    window.localStorage.setItem('blogUser', JSON.stringify(user)) 
     //noteService.setToken(user.token)    
     setUser(user)
     setNewUsername('')
     setNewPassword('')
+  }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
 
@@ -47,7 +61,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p> {user.name} logged in</p>
+      <p> {user.name} logged in <button onClick={handleLogout}>logout</button> </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
